036565c5aaf80327be5993f036debac3d5302a50
KaiKaixxxl <863217717@qq.com>
2022-07-25 22:55:30 +0800

    日常更新

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我显示学校名称</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码（数据、方法）
+    name: 'School'
+    export default {
+            data() {
+                return {
+                    name: 'atguigu',
+                    address: '上海'
+                }
+            },
+            methods: {
+                showName() {
+                    alert(this.name)
+                }
+            },
+        }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo {
+        background-color: orange;
+    }
+</style>
